1c11e94f88a9e31c23cb09111c83865a06ce485f
vikas verma <tovikasverma@outlook.com>
2020-06-06 14:50:23 +0530

    Eng of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,6 +29,13 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://locahost:4200");
+                });
+            });
 
         }
 
@@ -42,6 +49,7 @@ namespace API
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
