95f4c19cba38a3b7da6509caf45ecb8cb54fc056
vikas verma <tovikasverma@outlook.com>
2020-06-07 18:29:06 +0530

    End of section 8.

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,7 +33,7 @@ namespace API
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://locahost:4200");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
                 });
             });
 
